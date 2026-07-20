a007b5d6e05c16362fd6029861c9a011cee1acc9
Juan Gabriel Borsacchi Marques <juanborsacchi2006@gmail.com>
2026-07-20 18:13:47 -0300

    Diary Filter Implementation

---
+++ Journal/Models/RowContent.swift
@@ -0,0 +1,18 @@
+//
+//  RowContent.swift
+//  Journal
+//
+//  Created by Juan Gabriel Borsacchi Marques on 20/07/26.
+//
+
+import SwiftUI
+
+struct RowContent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RowContent()
+}
