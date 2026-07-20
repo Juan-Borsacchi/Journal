2adc32dc59a96c75a7081e793dc3fb78581f00b5
Juan Gabriel Borsacchi Marques <juanborsacchi2006@gmail.com>
2026-07-20 18:14:12 -0300

    Diary Filter Implementation

--- Journal/Models/RowContent.swift
+++ Journal/Models/RowContent.swift
@@ -8,11 +8,17 @@
 import SwiftUI
 
 struct RowContent: View {
+    let item: Register
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    RowContent()
+        VStack(alignment: .leading) {
+            Text(item.title)
+                .font(item.subtitle == nil ? .body : .headline)
+            if let subtitle = item.subtitle {
+                Text(subtitle)
+                    .font(.subheadline)
+            }
+        }
+        .badge(item.lock == true ? "\(Image(systemName: "lock"))" : "")
+        .badge(item.subtitle == nil ? "\(Image(systemName: "chevron.right"))" : "")    }
 }
